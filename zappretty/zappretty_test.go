19e2ec41d54ff1b1297687d31095eabe2e31ebf5
Robert Deusser <robertjdeusser@gmail.com>
2022-05-02 12:38:12 -0400

    chore: cleanup zappretty tests

--- zappretty/zappretty_test.go
+++ zappretty/zappretty_test.go
@@ -2,6 +2,7 @@ package zappretty
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 	"time"
@@ -15,9 +16,15 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	epoch     = time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
-	testcases = []struct {
+var epoch = time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+func TestMain(m *testing.M) {
+	color.NoColor = false
+	goleak.VerifyTestMain(m)
+}
+
+func TestPrettyOutput(t *testing.T) {
+	testcases := []struct {
 		name  string
 		entry zapcore.Entry
 		want  string
@@ -40,14 +47,7 @@ var (
 			want: fmt.Sprintf("\x1b[37m[%s]\x1b[0m \x1b[32mINFO \x1b[0m \x1b[90mmain\x1b[0m \x1b[90m(foo.go:42)\x1b[0m \x1b[97mhello world\x1b[0m \n", epoch.Format(timeFormat)),
 		},
 	}
-)
 
-func TestMain(m *testing.M) {
-	color.NoColor = false
-	goleak.VerifyTestMain(m)
-}
-
-func TestPrettyOutput(t *testing.T) {
 	for _, tc := range testcases {
 		encoder := NewCLIEncoder(EncoderTestEncoderConfig())
 
@@ -60,29 +60,28 @@ func TestPrettyOutput(t *testing.T) {
 }
 
 func TestFuzzLog(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
 	atom := zap.NewAtomicLevel()
 	cfg := zap.NewProductionEncoderConfig()
 
 	Register(cfg)
 
 	cliEncoder := NewCLIEncoder(cfg)
-	// jsonEncoder := zapcore.NewJSONEncoder(cfg)
+	jsonEncoder := zapcore.NewJSONEncoder(cfg)
 
-	// writer, closer, err := zap.Open(filepath.Join(osutil.TempDir(), "test.log"))
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// defer closer()
+	tmp, err := os.CreateTemp("", "test.*.log")
+	assert.NoError(t, err)
+
+	writer, closer, err := zap.Open(tmp.Name())
+	assert.NoError(t, err)
+	defer closer()
 
 	leveler := zap.LevelEnablerFunc(func(level zapcore.Level) bool {
 		return level >= atom.Level()
 	})
 
 	core := zapcore.NewTee(
-		zapcore.NewCore(cliEncoder, zapcore.AddSync(os.Stdout), leveler),
-		// zapcore.NewCore(jsonEncoder, os.Stdout, leveler),
+		zapcore.NewCore(cliEncoder, zapcore.AddSync(ioutil.Discard), leveler),
+		zapcore.NewCore(jsonEncoder, writer, leveler),
 	)
 
 	logger := zap.New(core).Named("zappretty")
@@ -99,12 +98,6 @@ func TestFuzzLog(t *testing.T) {
 		corpus = append(corpus, s)
 	}
 
-	defer func() {
-		if r := recover(); r != nil {
-			t.Fatal(r)
-		}
-	}()
-
 	go func() {
 		for i := 0; i < 1000; i++ {
 			logger.Info(corpus[i])
@@ -115,30 +108,3 @@ func TestFuzzLog(t *testing.T) {
 		logger.Info(corpus[i])
 	}
 }
-
-/*
-func FuzzPrettyOutput(f *testing.F) {
-	encoder := NewCLIEncoder(EncoderTestEncoderConfig())
-
-	for _, tc := range testcases {
-		out, err := encoder.EncodeEntry(tc.entry, nil)
-		if err != nil {
-			f.Fatalf("Encoding entry: %v", err)
-		}
-
-		f.Add(out.Bytes())
-	}
-
-	f.Fuzz(func(t *testing.T, data []byte) {
-		var entry zapcore.Entry
-
-		entryFuzz := gofuzz.New()
-		entryFuzz.Fuzz(&entry)
-
-		_, err := encoder.EncodeEntry(entry, nil)
-		if err != nil {
-			t.Fatalf("Encoding generated entry: %v", err)
-		}
-	})
-}
-*/
